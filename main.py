0f6ee19bf63d741ffe70abbe0a0849ffb14dee65
unknown <lizhenyang926@gmail.com>
2024-11-05 22:42:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# THE BIO FUNCTION
+def bio(name,born,pronoun):
+    turn=2024-born
+    future=2051-born
+    print(name.capitalize(),"was bron in ",born)
+    print(pronoun,"will return ",turn,"years old this year")
+    print(pronoun,"will be",future,"in the year 2051")
+    print()
+
+
+
+# PRINT BIOS
+bio("stephen",1984,"he")
+bio("mary",1990,"she")
+bio("jane",2000,"she")
